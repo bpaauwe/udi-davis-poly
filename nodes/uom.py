b9f0027aaf838998ac0cf877e1688aa3fa638c0d
Bob Paauwe <bpaauwe@yahoo.com>
2020-04-04 11:07:42 -0700

    Set correct UOM for the various fields.

--- nodes/uom.py
+++ nodes/uom.py
@@ -24,63 +24,30 @@ def get_uom(units):
             'DEWPT': 4,     # dew point
             'SOLRAD': 74,   # solar radiation
             'RAINRT': 46,   # rain rate
-            'GV0': 4,       # max temp
-            'GV1': 4,       # min temp
-            'GV2': 4,       # ??feels like
-            'GV3': 4,       # heat index  
-            'GV4': 4,       # wind chill
             'SPEED': 49,    # wind speed
-            'GV5': 49,      # wind gusts
-            'GV6': 82,      # rain
-            'GV7': 49,      # wind max
-            'GV8': 49,      # wind min  
-            'GV9': 56,      # moon phase
-            'GV10': 56,     # ozone
-            'GV11': 25,     # climate coverage
-            'GV12': 25,     # climate intensity
-            'GV13': 25,     # climate conditions
-            'GV14': 22,     # cloud conditions
-            'GV15': 82,     # snow depth
             'DISTANC': 83,  # visibility (kilometers)
             'UV': 71,       # UV index
-            'GV17': 56,     # Air Quality
-            'GV18': 22,     # chance of precipitation
-            'GV19': 25,     # day of week
-            'GV20': 106,    # ETo
-        }
-    elif unit_cfg == 'uk':
-        uom = {
-            'ST': 2,   # node server status
-            'CLITEMP': 4,   # temperature
-            'CLIHUM': 22,   # humidity
-            'BARPRES': 117, # pressure
-            'WINDDIR': 76,  # direction
-            'DEWPT': 4,     # dew point
-            'SOLRAD': 74,   # solar radiation
-            'RAINRT': 24,   # rain rate
             'GV0': 4,       # max temp
             'GV1': 4,       # min temp
-            'GV2': 4,       # feels like
-            'GV3': 4,       # ??feels like
-            'GV4': 4,       # wind chill
-            'SPEED': 48,    # wind speed
-            'GV5': 48,      # wind gusts
-            'GV6': 105,     # rain
-            'GV7': 48,      # max wind
-            'GV8': 48,      # min wind  
-            'GV9': 56,      # moon phase
-            'GV10': 56,     # ozone
-            'GV11': 25,     # climate coverage
-            'GV12': 25,     # climate intensity
-            'GV13': 25,     # climate conditions
-            'GV14': 22,     # cloud conditions
-            'GV15': 105,    # snow depth
-            'DISTANC': 116, # visibility
-            'UV': 71,       # UV index
-            'GV17': 56,     # Air Quality
-            'GV18': 22,     # chance of precipitation
-            'GV19': 25,     # day of week
-            'GV20': 120,    # ETo
+            'GV2': 4,       # max dewpoint
+            'GV3': 4,       # min dewpoint
+            'GV4': 4,       # heat index
+            'GV5': 4,       # windchill
+            'GV6': 82,      # n/a 
+            'GV7': 49,      # n/a 
+            'GV8': 22,      # humidity high
+            'GV9': 22,      # humidity low
+            'GV10': 117,    # pressure high
+            'GV11': 117,    # pressure low
+            'GV12': 82,     # rain
+            'GV13': 49,     # gusts
+            'GV14': 22,     # N/A
+            'GV15': 82,     # N/A
+            'GV16': 25,     # pressure trend
+            'GV17': 56,     # N/A
+            'GV18': 22,     # N/A
+            'GV19': 25,     # N/A
+            'GV20': 106,    # ETo
         }
     else:
         uom = {
@@ -92,28 +59,29 @@ def get_uom(units):
             'DEWPT': 17,    # dew point
             'SOLRAD': 74,   # solar radiation
             'RAINRT': 24,   # rain rate
-            'GV0': 17,      # max temp
-            'GV1': 17,      # min temp
-            'GV2': 17,      # feels like
-            'GV3': 17,      # ??feels like
-            'GV4': 17,      # wind chill
             'SPEED': 48,    # wind speed
-            'GV5': 48,      # wind gusts
-            'GV6': 105,     # rain
-            'GV7': 48,      # max wind
-            'GV8': 48,      # min wind   
-            'GV9': 56,      # moon phase
-            'GV10': 56,     # ozone
-            'GV11': 25,     # climate coverage
-            'GV12': 25,     # climate intensity
-            'GV13': 25,     # climate conditions
-            'GV14': 22,     # cloud conditions
-            'GV15': 105,    # snow depth
             'DISTANC': 116, # visibility
             'UV': 71,       # UV index
-            'GV17': 56,     # Air Quality
-            'GV18': 22,     # chance of precipitation
-            'GV19': 25,     # day of week
+            'GV0': 17,      # max temp
+            'GV1': 17,      # min temp
+            'GV2': 17,      # max dewpoint
+            'GV3': 17,      # min dewpoint
+            'GV4': 17,      # heat index
+            'GV5': 17,      # windchill
+            'GV6': 105,     # N/A
+            'GV7': 48,      # N/A
+            'GV8': 22,      # min humidity
+            'GV9': 22,      # max humidity
+            'GV10': 23,     # min pressure
+            'GV11': 23,     # max pressure
+            'GV12': 105,    # precipitation
+            'GV13': 48,     # gust
+            'GV14': 22,     # N/A
+            'GV15': 105,    # N/A
+            'GV16': 25,     # pressure trend
+            'GV17': 56,     # N/A
+            'GV18': 22,     # N/A
+            'GV19': 25,     # N/A
             'GV20': 120,    # ETo
         }
 
