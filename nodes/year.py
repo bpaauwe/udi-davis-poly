7956c18412ae6be11b398f737629b110fc62679c
Bob Paauwe <bpaauwe@yahoo.com>
2020-04-04 10:02:55 -0700

    Add parsing for year data.

--- nodes/year.py
+++ nodes/year.py
@@ -9,13 +9,17 @@ import time
 import datetime
 import json
 import math
+import node_funcs
+from nodes import uom
 
 LOGGER = polyinterface.LOGGER
 
+@node_funcs.add_functions_as_methods(node_funcs.functions)
 class YearNode(polyinterface.Node):
     id = 'year'
     hint = [1,11,4,0]
     units = 'us'
+    uom = {}
     drivers = [
             {'driver': 'GV0', 'value': 0, 'uom': 17}, # temp high
             {'driver': 'GV1', 'value': 0, 'uom': 17}, # temp low
@@ -35,11 +39,34 @@ class YearNode(polyinterface.Node):
             {'driver': 'GV20', 'value': 0, 'uom': 120}, # ETo
             ]
 
+
     def SetUnits(self, units):
         LOGGER.debug('set units info')
+        self.uom = uom.get_uom(self.units)
 
     def parse(self, jdata):
         LOGGER.debug('Parse jdata for year data here')
+        try:
+            obs = jdata['davis_current_observation']
+            LOGGER.debug(obs)
+            self.update_driver('GV0', obs['temp_year_high_f'])
+            self.update_driver('GV1', obs['temp_year_low_f'])
+            self.update_driver('GV2', obs['dewpoint_year_high_f'])
+            self.update_driver('GV3', obs['dewpoint_year_low_f'])
+            self.update_driver('GV4', obs['heat_index_year_high_f'])
+            self.update_driver('GV5', obs['windchill_year_low_f'])
+            self.update_driver('GV8', obs['relative_humidity_year_high'])
+            self.update_driver('GV9', obs['relative_humidity_year_low'])
+            self.update_driver('GV10', obs['pressure_year_high_in'])
+            self.update_driver('GV11', obs['pressure_year_low_in'])
+            self.update_driver('GV12', obs['rain_year_in'])
+            self.update_driver('RAINRT', obs['rain_rate_year_high_in_per_hr'])
+            self.update_driver('SPEED', obs['wind_year_high_mph'])
+            self.update_driver('SOLRAD', obs['solar_radiation_year_high'])
+            self.update_driver('UV', obs['uv_index_year_high'])
+            self.update_driver('GV20', obs['et_year'])
+        except Exception as e:
+            LOGGER.error('Parse failure for year: ' + str(e))
 
 
 
